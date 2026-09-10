4e5aac5df2b23af92d9f723cd63670b62a73a39a
renya <bza11074@nifty.com>
2026-09-10 19:27:52 +0900

    change

--- scripts/build-tags.js
+++ scripts/build-tags.js
@@ -131,7 +131,7 @@ if (linkedTags.has(tagName)) {
   <h1>${tagName}</h1>
 
 <p class="description">
-  <a href="/pages/${tagName}/">${tagName}</a>タグが付いたページリストを表示しています。<br>
+  suwa.infoの<a href="/pages/${tagName}/">${tagName}</a>タグが付いたページリストを表示しています。<br>
   ${uniqueItems.length}ページ
 </p>
 
